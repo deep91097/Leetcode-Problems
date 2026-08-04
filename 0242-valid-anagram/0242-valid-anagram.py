f7557022f20818b324ec34659f4e051440145a67
Deep Kumar Prasad <68179294+deep91097@users.noreply.github.com>
2026-08-04 16:23:11 +0530

    Time: 8 ms (82.81%), Space: 19.6 MB (30.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,17 +3,18 @@ class Solution:
         # s = sorted(s)
         # t = sorted(t)
         # return s==t
-        if len(s) != len(t):
-            return False
-        countS, countT= {}, {}
+        # if len(s) != len(t):
+        #     return False
+        # countS, countT= {}, {}
 
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
-            countT[t[i]] = 1 + countT.get(t[i],0)
+        # for i in range(len(s)):
+        #     countS[s[i]] = 1 + countS.get(s[i],0)
+        #     countT[t[i]] = 1 + countT.get(t[i],0)
 
-        for c in countS:
-            if countS[c] != countT.get(c,0):
-                return False
+        # for c in countS:
+        #     if countS[c] != countT.get(c,0):
+        #         return False
             
-        return True
+        # return True
+        return Counter(s) == Counter(t)
         
