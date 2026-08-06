3017c424f8fb64b07bce30a37550a733f18a3b82
Deep Kumar Prasad <68179294+deep91097@users.noreply.github.com>
2026-08-06 09:02:15 +0530

    Time: 609 ms (44.33%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,14 +1,12 @@
-/* Write your T-SQL query statement below */
-
-select t.request_at as 'Day', 
-round(SUM(CASE When t.status like 'cancelled%' then 1 else 0 end)*1.0
+# Write your MySQL query statement below
+select 
+t.request_at as Day, 
+round(sum(case when status like 'cancelled%' then 1 else 0 end) * 1.0
 /
 count(*),2) as 'Cancellation Rate'
-
-from Trips t left join Users u1
-on t.client_id = u1.users_id
-left join Users u2
-on t.driver_id = u2.users_id
+from Trips t
+left join Users u1 on t.client_id = u1.users_id
+left join Users u2 on t.driver_id = u2.users_id
 where u1.banned = 'No' and u2.banned = 'No'
 and request_at between '2013-10-01' and '2013-10-03'
 group by t.request_at
