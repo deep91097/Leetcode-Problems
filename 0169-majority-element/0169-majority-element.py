0d89406c92885f0684c2a92b0f683c3494b988d7
Deep Kumar Prasad <68179294+deep91097@users.noreply.github.com>
2026-08-15 16:25:55 +0530

    Time: 3 ms (84.8%), Space: 21 MB (84.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = {} #Hashmap for storing the actual value in the array nums and their count
-        res, maxCount = 0,0 # variables to keep track of res and MaxCount of that value
+        # count = {} #Hashmap for storing the actual value in the array nums and their count
+        # res, maxCount = 0,0 # variables to keep track of res and MaxCount of that value
+        # for n in nums:
+        #     count[n] = 1 + count.get(n, 0)
+        #     res = n if count[n] > maxCount else res
+        #     maxCount = max(count[n],maxCount)
+        # return res
+
+        res, count = 0, 0
         for n in nums:
-            count[n] = 1 + count.get(n, 0)
-            res = n if count[n] > maxCount else res
-            maxCount = max(count[n],maxCount)
+            if count == 0:
+                res = n
+            count += 1 if res == n else -1
         return res
 
         
