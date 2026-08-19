7875a4edf7e899e41bee676aaab443f8981f2058
Deep Kumar Prasad <68179294+deep91097@users.noreply.github.com>
2026-08-19 08:47:33 +0530

    Time: 55 ms (37.4%), Space: 25.2 MB (44.42%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -3,41 +3,38 @@ class ListNode:
         self.key = key
         self.next = None
 
+
 class MyHashSet:
 
     def __init__(self):
         self.set = [ListNode(0) for i in range(10**4)]
         
-
     def add(self, key: int) -> None:
-        cur_pointer = self.set[key % len(self.set)]
-        while cur_pointer.next:
-            if cur_pointer.next.key == key:
-                return
-
-            cur_pointer = cur_pointer.next
-
-        cur_pointer.next = ListNode(key)    
-
+        cur = self.set[key % len(self.set)]
+        while cur.next:
+            if cur.next.key == key:
+                return 
+            cur = cur.next
+        cur.next = ListNode(key)
         
-
     def remove(self, key: int) -> None:
-        cur_pointer = self.set[key % len(self.set)]
-        while cur_pointer.next:
-            if cur_pointer.next.key == key:
-                cur_pointer.next = cur_pointer.next.next 
+        cur = self.set[key % len(self.set)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
                 return
-
-            cur_pointer = cur_pointer.next
-
+            cur = cur.next
+        
+        
     def contains(self, key: int) -> bool:
-        cur_pointer = self.set[key % len(self.set)]
-        while cur_pointer.next:
-            if cur_pointer.next.key == key:
+        cur = self.set[key % len(self.set)]
+        while cur.next:
+            if cur.next.key == key:
                 return True
-
-            cur_pointer = cur_pointer.next
+            cur = cur.next
+        
         return False
+      
         
 
 
