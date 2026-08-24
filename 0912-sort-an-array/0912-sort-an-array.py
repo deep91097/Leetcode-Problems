c6d28b454acb606ff7f7417b84b24def4f6bd1ff
Deep Kumar Prasad <68179294+deep91097@users.noreply.github.com>
2026-08-24 08:19:56 +0530

    Time: 763 ms (25.97%), Space: 26.1 MB (93.7%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,39 +1,40 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-
-        def merge(arr,L,M,R):
+        def merge(arr, L, M, R):
             left, right = arr[L:M+1], arr[M+1:R+1]
-            i,j,k = L,0,0
+            i, j, k = L, 0, 0
 
-            while j < len(left) and k < len(right):
+            while j< len(left) and k< len(right):
                 if left[j] <= right[k]:
                     arr[i] = left[j]
-                    j+=1
+                    j += 1
                 else:
-                    arr[i] =right[k]
-                    k+=1
-
-                i+=1
-
+                    arr[i] = right[k]
+                    k += 1
+                i += 1
+            
             while j < len(left):
                 arr[i] = left[j]
-                j+=1
-                i+=1
-            
+                j += 1
+                i += 1
+
             while k < len(right):
                 arr[i] = right[k]
-                k+=1
-                i+=1
+                k += 1
+                i += 1
             
 
-
         def mergeSort(arr,l,r):
-            if l >= r:
+            if l == r:
                 return arr
-            m = (l+r) // 2
-            mergeSort(arr, l,m)
-            mergeSort(arr, m+1,r)
+            
+            m = (l + r) // 2
+            mergeSort(arr,l,m)
+            mergeSort(arr,m+1,r)
             merge(arr,l,m,r)
             return arr
-        return mergeSort(nums, 0, len(nums)-1)
+
+        return mergeSort(nums,0,len(nums)-1)
+
+
         
